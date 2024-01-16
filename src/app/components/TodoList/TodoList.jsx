2fe953d9ac1b053017d8ce87f7cbf6b2e1574a22
Rocío Cejudo <rocio.cejudo.soronellas@gmail.com>
2024-01-16 21:20:20 +0100

    add json server in the project

---
+++ src/app/components/TodoList/TodoList.jsx
@@ -0,0 +1,21 @@
+export default function TodoList() {
+  return (
+    <div className="overflow-x-auto">
+      <table className="table">
+
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Job</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Cy Ganderton</td>
+            <td>Quality Control Specialist</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import AddTask from './components/AddTask/AddTask'
+import TodoList from './components/TodoList/TodoList'
+
+export default function Home() {
+  return (
+    <main className='max-w-4xl mx-auto mt-4'>
+      <div className='text-center my-5 flex flex-col gap-4'>
+        <h1 className='text-2xl font-bold'>Todo list App</h1>
+        <AddTask />
+      </div>
+      <TodoList />
+    </main>
+  )
+}
